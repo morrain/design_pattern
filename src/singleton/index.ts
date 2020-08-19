ebd7e7a91d519f883b53ab88c2549787fc770146
kongchuiliang <626100676@qq.com>
2020-08-19 15:43:28 +0800

    feat: 初始化工程化设施

--- src/singleton/index.ts
+++ src/singleton/index.ts
@@ -1,6 +1,8 @@
 class Singleton {
     private static instance: Singleton;
-    private constructor () { }
+    private constructor () { 
+        
+    }
     public static getInstance(): Singleton {
       if(!Singleton.instance){
          Singleton.instance = new Singleton()
